8c23b3948f8d2bffc6a35b1b2ed28514513c8d41
CPDrogas <coordepesquisacpdrogas@gmail.com>
2023-06-01 16:30:29 -0300

    first step

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'login-project';
+
+$dsn = 'mysql:host=' . $host . '; dbname' . $dbname;
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ)
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connect successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
