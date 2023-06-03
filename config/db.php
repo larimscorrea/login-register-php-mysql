c93d51b019e3f07c6681472d8f3228077b4c9d13
Larissa Corrẽa <larimscorrea@gmail.com>
2023-06-03 18:01:43 -0300

    more a step

--- config/db.php
+++ config/db.php
@@ -4,11 +4,11 @@ $user = 'root';
 $password = '';
 $dbname = 'login-project';
 
-$dsn = 'mysql:host=' . $host . '; dbname' . $dbname;
+$dsn = 'mysql:host=' . $host . ';dbname' . $dbname;
 
 try {
     $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ)
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connect successfully";
 } catch(PDOException $e) {
---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+require('./config/db.php')
+?>
+
+<?php require('./inc/header.html'); ?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header bg-light mb-3">Registre-se
+            <div class="card-body">
+                <h5>Por favor, faça seu login ou registre-se para ter acesso a todo conteúdo.</h5>
+            </div>
+        </div>
+
+    </div>
+
+    <?php require('./inc/footer.html') ?>
