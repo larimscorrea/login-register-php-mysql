71a1fa9d4a9a3bf7d3db8fb9947602ba0ab513a5
Larissa Corrẽa <larimscorrea@gmail.com>
2023-06-03 16:44:21 -0300

    more a step

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php 
-    echo "Hello, world";
+    require('./config/db.php');
 ?> 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php require('./inc/header.html'); ?>
+
+<div class="container">
+
+    <div class="card bg-light mb-3">
+
+    <div class="card-header">
+    <h5>Seja bem-vindo, visitante</h5>
+    </div>
+
+    <div class="card-body">
+    <h5>Por favor faça seu login ou registre-se</h5>
+    </div>
+
+    </div>
+
+</div>
+
+<?php require('./inc/footer.html'); ?>
